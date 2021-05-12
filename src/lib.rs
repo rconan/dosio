1d2d7a01e0aa6f44dcf512b121cd264be4d47fa4
Rod Conan <rconan@gmto.org>
2021-05-12 16:51:38 -0700

    DOSIODATA TYPE
    Same type for both inputs & outputs,
    backward incompatible for inputs dur to the wrapping into an Option

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,13 @@ pub mod io;
 pub use error::DOSIOSError;
 pub use io::IO;
 
+pub type DosIoData = Option<Vec<IO<Vec<f64>>>>;
 /// Used to glue together the different components of an end-to-end model
-pub trait DOS {
+pub trait Dos {
     /// Computes and returns a vector outputs from a model component
-    fn outputs(&mut self) -> Option<Vec<IO<Vec<f64>>>>;
+    fn outputs(&mut self) -> DosIoData;
     /// Passes a vector of input data to a model component
-    fn inputs(&mut self, data: Vec<IO<Vec<f64>>>) -> Result<&mut Self, DOSIOSError>;
+    fn inputs(&mut self, data: DosIoData) -> Result<&mut Self, DOSIOSError>;
     /// Updates the state of a model component for one time step
     fn step(&mut self) -> Result<&mut Self, DOSIOSError>
     where
@@ -17,15 +18,12 @@ pub trait DOS {
         self.next()
             .and(Some(self))
             .ok_or_else(|| "DOS next step has issued None".into())
-            .map_err(|e| DOSIOSError::Step(e))
+            .map_err(DOSIOSError::Step)
     }
     /// Combines `inputs`, `step` and `outputs` in a single method
     ///
     /// This is equivalent to `.inputs(...)?.step()?.outputs()?`
-    fn in_step_out(
-        &mut self,
-        data: Vec<IO<Vec<f64>>>,
-    ) -> Result<Option<Vec<IO<Vec<f64>>>>, DOSIOSError>
+    fn in_step_out(&mut self, data: DosIoData) -> Result<DosIoData, DOSIOSError>
     where
         Self: Sized + Iterator,
     {
